1e705074cbe2acce62833a990b9d11e60ca6ab8f
jooya38 <hyj0102809@gmail.com>
2025-04-03 07:50:50 +0900

    FEAT: Create dietManagementWidget

---
+++ alpha_front/lib/widgets/dietManagement.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class DietManagementWidget extends StatefulWidget {
+  const DietManagementWidget({super.key});
+
+  @override
+  _DietManagementWidgetState createState() => _DietManagementWidgetState();
+}
+
+class _DietManagementWidgetState extends State<DietManagementWidget> {
+  List<String> foodImages = [
+    // "assets/food_0.png",
+    // "assets/food_1.png",
+    // "assets/food_2.png",
+    // "assets/food_3.png",
+    // "assets/food_4.png",
+    // "assets/food_5.png",
+  ]; // 서버에서 받아올 데이터
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 326,
+      height: 196,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.3), blurRadius: 8)
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Text("저녁",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+              const SizedBox(width: 4),
+              const Text("칼로리 비례 음식량이 부족합니다!"),
+              const SizedBox(width: 10),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Wrap(
+            spacing: 8,
+            children: foodImages.map((image) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(image, width: 50, height: 50),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
