739fb2da551be617b4311dac74ef52eeedce2c0b
jooya38 <hyj0102809@gmail.com>
2025-04-12 02:50:56 +0900

    FEAT: Create dateCircleButton in weekCal

--- alpha_front/lib/widgets/weekCal.dart
+++ alpha_front/lib/widgets/weekCal.dart
@@ -12,18 +12,111 @@ class Weekcal extends StatefulWidget {
 class _WeekcalState extends State<Weekcal> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        width: 376,
-        height: 66,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(33),
-          border: Border.all(
-            width: 3,
-            color: const Color.fromRGBO(60, 177, 150, 1.0),
-          ),
+    return Container(
+      width: 400,
+      height: 66,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(33),
+        border: Border.all(
+          width: 3,
+          color: const Color.fromRGBO(60, 177, 150, 1.0),
         ),
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "일",
+            onTap: () {
+              print("일요일 선택됨");
+            },
+            isSuccess: false,
+          ),
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "월",
+            onTap: () {
+              print("월요일 선택됨");
+            },
+            isSuccess: true,
+          ),
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "화",
+            onTap: () {
+              print("화요일 선택됨");
+            },
+            isSuccess: false,
+          ),
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "수",
+            onTap: () {
+              print("수요일 선택됨");
+            },
+            isSuccess: false,
+          ),
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "목",
+            onTap: () {
+              print("목요일 선택됨");
+            },
+            isSuccess: false,
+          ),
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "금",
+            onTap: () {
+              print("금요일 선택됨");
+            },
+            isSuccess: false,
+          ),
+          const SizedBox(width: 10),
+          dateCircleButton(
+            text: "토",
+            onTap: () {
+              print("토요일 선택됨");
+            },
+            isSuccess: false,
+          ),
+          const SizedBox(width: 10),
+        ],
       ),
     );
   }
+}
+
+Widget dateCircleButton({
+  required String text,
+  required VoidCallback onTap,
+  required bool isSuccess,
+}) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      width: 44,
+      height: 44,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color:
+            isSuccess ? const Color.fromRGBO(60, 177, 150, 1.0) : Colors.white,
+        border: Border.all(
+          color: const Color.fromRGBO(60, 177, 150, 1.0),
+          width: 3,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            fontFamily: "PretenderardVariable",
+            color: isSuccess ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    ),
+  );
 }
